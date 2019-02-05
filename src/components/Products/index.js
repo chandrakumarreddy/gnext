f8f7ee70fc701c716ea7f23d5bf6339bbc730b60
chandrakumarreddy <chandrapenugonda655@gmail.com>
2019-02-05 08:24:52 +0530

    version-03

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 import {Link,withRouter} from "react-router-dom";
 import { withStyles } from '@material-ui/core/styles';
+import {Button} from '@material-ui/core';
 import computers from '../../resources/Products/computers.jpg';
 
 const styles = theme => ({
@@ -24,6 +25,26 @@ const styles = theme => ({
     	fontSize:'11px',
     	color:'#333',
     	paddingLeft:'3px'
+    },
+    addToCart:{
+    	textTransform:'capitalize',
+    	backgroundColor:'yellow',
+    	border:'none',
+    	color:'#000',
+    	width:'90%',
+    	margin:'10px 0',
+    	fontSize:'12px',
+    	boxShadow:'none',
+    	outline:'none'
+    },
+    checkout:{
+    	textTransform:'capitalize',
+    	backgroundColor:'orange',
+    	border:'none',
+    	color:'#000',
+    	width:'90%',
+    	margin:'10px 0',
+    	fontSize:'12px'
     }
 });
 
@@ -46,10 +67,22 @@ const ProductsComponent=(props)=>{
 							<p>Shipping: Unavailable for 96939</p>
 							<p>Store Pickup: Available for pickup today at Aiea.</p>
 							<p>Act Quick – Only 2 left at your store!</p>
+							<p>
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z"/></svg>									
+							</p>
 							<p>Unfortunately, this item is only available for shipping in certain markets.</p>
 						</Grid>
 						<Grid item lg={3}>
 							<p>price :- $1,199.99</p>
+							<Button variant='contained' color='secondary' className={classes.addToCart}>
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/></svg>
+							&nbsp;&nbsp;&nbsp;Add to cart</Button>
+							<Button variant='contained' color='secondary' className={classes.checkout}>
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/></svg>								
+							&nbsp;&nbsp;&nbsp;Proceed to Checkout</Button>
 						</Grid>
 					</Grid>
 				</Link>
